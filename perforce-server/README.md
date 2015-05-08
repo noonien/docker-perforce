8b86b94e64e6c7698e74c2ec4be78cadb185821b
George Jiglau <george@mux.ro>
2015-05-08 15:30:02 +0300

    Change environment variable names to match with perforce

--- perforce-server/README.md
+++ perforce-server/README.md
@@ -7,16 +7,15 @@ This is a docker image for the [Perforce Helix Server](http://www.perforce.com/)
 Usage
 -----
 
-    docker run -e PERFORCE_SERVER_NAME=server-name -e PERFORCE_PASSWORD=<password> noonien/perforce-server
+    docker run -e SERVER_NAME=server-name -e P4PASSWD=<password> noonien/perforce-server
 
 Details
 -------
 The following environment variables are available:
 
-  - PERFORCE_SERVER_NAME - Server name. Required.
-  - PERFORCE_PROTOCOL - Server protocol. Described [here](http://www.perforce.com/perforce/doc.current/manuals/cmdref/P4PORT.html). Defaults to ssl.
-  - PERFORCE_PORT - Server port. Defaults to 1666.
-  - PERFORCE_USER - Superuser username. Only created when creating a new server. Defaults to perforce.
-  - PERFORCE_PASSWORD - Superuser password. Required when creating a new server.
+  - SERVER_NAME - Server name. Required.
+  - P4PORT - Address on which to listen. Described [here](http://www.perforce.com/perforce/doc.current/manuals/cmdref/P4PORT.html). Defaults to ssl:1666.
+  - P4USER - Superuser username. Only created when creating a new server. Defaults to perforce.
+  - P4PASSWd - Superuser password. Required when creating a new server.
 
 The path `/opt/perforce/server` is mounted as a volume because that's where the server roots are stored.
--- perforce-server/run.sh
+++ perforce-server/run.sh
@@ -3,45 +3,44 @@ set -e
 
 # Perforce paths
 CONFIGURE_SCRIPT=/opt/perforce/sbin/configure-perforce-server.sh
-PERFORCE_SERVERS_ROOT=/opt/perforce/servers
-PERFORCE_CONFIG_ROOT=/etc/perforce/p4dctl.conf.d
+SERVERS_ROOT=/opt/perforce/servers
+CONFIG_ROOT=/etc/perforce/p4dctl.conf.d
 
 # Theese need to be defined
-if [ -z "$PERFORCE_SERVER_NAME" ]; then
-    echo FATAL: PERFORCE_SERVER_NAME not defined 1>&2
+if [ -z "$SERVER_NAME" ]; then
+    echo FATAL: SERVER_NAME not defined 1>&2
     exit 1;
 fi
-if [ -z "$PERFORCE_PASSWORD" ]; then
-    echo FATAL: PERFORCE_PASSWORD not defined 1>&2
+if [ -z "$P4PASSWD" ]; then
+    echo FATAL: P4PASSWD not defined 1>&2
     exit 1;
 fi
 
 # Default values
-PERFORCE_USER=${PERFORCE_USER:-p4admin}
-PERFORCE_PROTOCOL=${PERFORCE_PROTOCOL:-ssl}
-PERFORCE_PORT=${PERFORCE_PORT:-1666}
+P4USER=${P4USER:-p4admin}
+P4PORT=${P4PORT:-ssl:1666}
 
 # Check if the server was configured. If not, configure it.
-if [ ! -f $PERFORCE_CONFIG_ROOT/$PERFORCE_SERVER_NAME.conf ]; then
-    echo Perforce server $PERFORCE_SERVER_NAME not configured, configuring.
+if [ ! -f $CONFIG_ROOT/$SERVER_NAME.conf ]; then
+    echo Perforce server $SERVER_NAME not configured, configuring.
 
     # If the root path already exists, we're configuring an existing server
-    PERFORCE_SERVER_ROOT=$PERFORCE_SERVERS_ROOT/$PERFORCE_SERVER_NAME
+    SERVER_ROOT=$SERVERS_ROOT/$SERVER_NAME
     $CONFIGURE_SCRIPT -n \
-        -r $PERFORCE_SERVER_ROOT \
-        -p $PERFORCE_PROTOCOL:$PERFORCE_PORT \
-        -u $PERFORCE_USER \
-        -P $PERFORCE_PASSWORD \
-        $PERFORCE_SERVER_NAME
+        -r $SERVER_ROOT \
+        -p $P4PORT \
+        -u $P4USER \
+        -P $P4PASSWD \
+        $SERVER_NAME
 
     echo Server info:
-    p4 -p $PERFORCE_PROTOCOL:$PERFORCE_PORT info
+    p4 info
 else
     # Configuring the server also starts it, if we've not just configured a
     # server, we need to start it ourselves.
-    p4dctl start ${PERFORCE_SERVER_NAME}
+    p4dctl start $SERVER_NAME
 fi
 
 # Pipe server log and wait until the server dies
-PID_FILE=/var/run/p4d.$PERFORCE_SERVER_NAME.pid
-exec /usr/bin/tail --pid=$(cat $PID_FILE) -n 0 -f "$PERFORCE_SERVER_ROOT/log"
+PID_FILE=/var/run/p4d.$SERVER_NAME.pid
+exec /usr/bin/tail --pid=$(cat $PID_FILE) -n 0 -f "$SERVER_ROOT/log"
